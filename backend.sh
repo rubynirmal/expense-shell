50aad778f5ce375a3fa767dcf808d49657badd91
NirmalRuby <nnirmal144@gmail.com>
2023-11-07 16:39:24 +0530

    expense.conf project #comments #print(echo)

--- backend.sh
+++ backend.sh
@@ -1,28 +1,58 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
+log_file="/tmp/expense.log"
+color="\e[33m"
 
-dnf install nodejs -y
-
-cp backend-dev.service /etc/systemd/system/backend.service
-
-useradd expense
-
-mkdir /app
-
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-cd /app
-unzip /tmp/backend.zip
-
-cd /app
-npm install
-
-dnf install mysql -y
-
-mysql -h mysql-dev.rubydevops.cloud -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo -e "${color} Disable NodeJS default version \e[0m"
+dnf module disable nodejs -y &>>log_file
 
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+echo -e "${color} Enable NodeJS 18 Version \e[0m"
+dnf module enable nodejs:18 -y &>>log_file
+echo &?
 
+echo -e "${color} install NodeJS  \e[0m"
+dnf install nodejs -y &>>log_file
+echo $?
+
+echo -e "${color} copy Backend Service File \e[0m"
+cp backend-dev.service /etc/systemd/system/backend.service &>>log_file
+echo $?
+
+echo -e"${color} Add Application user \e[0m"
+useradd expense &>>log_file
+echo $?
+
+echo -e "${color} Create Application Directory \e[0m"
+mkdir /app &>>log_file
+echo $?
+
+echo -e "${color} Delete old Application content \e[0m"
+rm -rf /app/* &>>log_file
+echo $?
+
+echo -e "${color} Download Application content \e[0m"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
+echo $?
+
+echo -e "${color} Extract Application content \e[0m"
+cd /app  &>>log_file
+unzip /tmp/backend.zip  &>>log_file
+echo $?
+
+echo -e "${color} Download NodeJS Dependencies \e[0m"
+npm install &>>log_file
+echo $?
+
+echo -e "${color} Install MySQL Client to Load Schema \e[0m"
+dnf install &>>log_file
+echo $?
+
+echo -e "${color}  Load Schema \e[0m"
+mysql -h mysql-dev.rubydevops.cloud -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
+echo $?
+
+echo -e "${color} Starting Backend Service \e[0m"
+systemctl daemon-reload &>>log_file
+systemctl enable backend &>>log_file
+systemctl start backend  &>>log_filels
+echo $?
 
--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,28 @@
-dnf install nginx -y
+log_file="/tmp/expense.log"
+color="\e[33m"
 
- cp expense.conf /etc/nginx/default.d/expense.conf
+echo -e "${color} installing Nginx \e[0m"
+dnf install nginx -y &>>$$log_file
+echo $?
 
+echo -e "${color} Copy Expense Config file \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
+echo $?
 
-rm -rf /usr/share/nginx/html/*
+echo -e"${color} Download Frontend Application code \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $?
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo 0e "${color} Download Frontend Apllication code \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo $?
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+echo -e "${color} Extract downloaded Application content \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>log_file
+echo $?
 
-cp expense.conf /etc/nginx/default.d/expense.conf
-
-systemctl enable nginx
-systemctl start nginx
+echo -e "{color} Starting Nginx Service \e[0m"
+systemctl enable nginx &>>$log_file
+systemctl start nginx &>>log_file
+echo $?
