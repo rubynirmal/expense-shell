05f4869948d5fed1886f070d1e506b4fa2a92dec
NirmalRuby <nnirmal144@gmail.com>
2023-11-08 12:32:51 +0530

    expense.conf project #comments #print(echo)

--- mysql-dev.sh
+++ mysql-dev.sh
@@ -1,13 +1,26 @@
-dnf module disable mysql -y
+log_file="/tmp/expense.log"
+color="\e[33m"
 
-cp mysql-dev.repo /etc/yum.repos.d/mysql.repo
+echo -e "${color} Disable Mysql default version \e[0m"
+dnf module disable mysql -y &>>$log_file
+echo $?
 
-dnf install mysql-community-server -y
+echo -e "${color} copy Mysql repo file \e[0m"
+cp mysql-dev.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+echo $?
 
-systemctl enable mysqld
-systemctl start mysqld
+echo -e "${color} install MySQL sever \e[0m"
+dnf install mysql-community-server -y &>>$log_file
+echo $?
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo -e "${color} Start MySQL server \e[0m"
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld  &>>$log_file
+echo $?
+
+echo -e "${color} set MySQL password \e[0m"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
+echo $?
 
 
 
