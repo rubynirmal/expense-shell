b6511a6ec77fbff3d398dcfa69ee3488c30ee9a7
NirmalRuby <nnirmal144@gmail.com>
2023-11-08 12:44:10 +0530

    expense.conf project #comments #print(echo)

--- frontend.sh
+++ frontend.sh
@@ -9,11 +9,11 @@ echo -e "${color} Copy Expense Config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
 echo $?
 
-echo -e"${color} Download Frontend Application code \e[0m"
+echo -e "${color} Download Frontend Application code \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
 echo $?
 
-echo 0e "${color} Download Frontend Apllication code \e[0m"
+echo -e "${color} Download Frontend Apllication code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 echo $?
 
@@ -22,7 +22,7 @@ cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>log_file
 echo $?
 
-echo -e "{color} Starting Nginx Service \e[0m"
+echo -e "${color} Starting Nginx Service \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl start nginx &>>log_file
 echo $?
