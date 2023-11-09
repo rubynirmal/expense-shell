205f8590d4879fd0552f93c15563db76ae1095a3
NirmalRuby <nnirmal144@gmail.com>
2023-11-09 11:26:48 +0530

    expense.conf project #comments #print(echo)

--- frontend.sh
+++ frontend.sh
@@ -1,28 +1,27 @@
-log_file="/tmp/expense.log"
-color="\e[33m"
+source common.sh
 
 echo -e "${color} installing Nginx \e[0m"
 dnf install nginx -y &>>$$log_file
-echo $?
+status_check
 
 echo -e "${color} Copy Expense Config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
-echo $?
+status_check
 
 echo -e "${color} Download Frontend Application code \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Download Frontend Apllication code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Extract downloaded Application content \e[0m"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>log_file
-echo $?
+status_check
 
 echo -e "${color} Starting Nginx Service \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl start nginx &>>log_file
-echo $?
+status_check
--- mysql.sh
+++ mysql.sh
@@ -1,26 +1,31 @@
-log_file="/tmp/expense.log"
-color="\e[33m"
+source common.sh
+
+if [ -z "$1" ]; then
+  echo password intput missing
+  exit
+ f1
+ MYSQL_ROOT_PASSWORD=$1
 
 echo -e "${color} Disable Mysql default version \e[0m"
 dnf module disable mysql -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} copy Mysql repo file \e[0m"
 cp mysql-dev.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} install MySQL sever \e[0m"
 dnf install mysql-community-server -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Start MySQL server \e[0m"
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld  &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} set MySQL password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-echo $?
+mysql_secure_installation --set-root-pass ${MYSQL_ROOT_PASSWORD} &>>$log_file
+status_check
 
 
 
