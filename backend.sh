7765d1747f6904496344eb6023eed08c6f033ef4
NirmalRuby <nnirmal144@gmail.com>
2023-11-09 07:53:57 +0530

    expense.conf project #comments #print(echo)

--- backend.sh
+++ backend.sh
@@ -1,116 +1,80 @@
 log_file="/tmp/expense.log"
 color="\e[33m"
 
+if [ -z "$1" ]; then
+  echo password intput missing
+  exit
+ f1
+
+MYSQL_ROOT_PASSWORD-$1
+
+status check () {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+   else
+     echo -e "\e[31m FAILURE \e[0m"
+  fi
+  }
+
 echo -e "${color} Disable NodeJS default version \e[0m"
 dnf module disable nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
+
 
 
 echo -e "${color} Enable NodeJS 18 Version \e[0m"
 dnf module enable nodejs:18 -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
 
 echo -e "${color} install NodeJS  \e[0m"
 dnf install nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
 
 echo -e "${color} copy Backend Service File \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
-else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
-
+status check
 
 id expense &>>$log_file
 if [ $? -ne 0 ]; then
   echo -e "${color} Add Application user \e[0m"
   useradd expense &>>$log_file
-    if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
 fi
 
 if [ ! -d /app ]; then
   echo -e "${color} Create Application Directory \e[0m"
   mkdir /app &>>$log_file
-   if [ $? -eq 0 ]; then
-     echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
- fi
+status check
 fi
 
 echo -e "${color} Delete old Application content \e[0m"
 rm -rf /app/* &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
 
 echo -e "${color} Download Application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
 
 echo -e "${color} Extract Application content \e[0m"
 cd /app  &>>log_file
 unzip /tmp/backend.zip  &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
+
 
 echo -e "${color} Download NodeJS Dependencies \e[0m"
 npm install &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
 
 echo -e "${color} Install MySQL Client to Load Schema \e[0m"
 dnf install mysql -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
 
 echo -e "${color}  Load Schema \e[0m"
 mysql -h mysql-dev.rubydevops.cloud -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
 
 echo -e "${color} Starting Backend Service \e[0m"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend  &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-   echo -e "\e[31m FAILURE \e[0m"
-fi
+status check
