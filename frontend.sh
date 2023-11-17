74f72e7a32c03db547b2a80338cdeda8df175e35
NirmalRuby <nnirmal144@gmail.com>
2023-11-17 10:38:25 +0530

    correction

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,27 @@
 source common.sh
 
 echo -e "${color} Installing Nginx \e[0m"
-dnf install nginx -y &>>"$log_file"
+dnf install nginx -y &>>$log_file
 status_check
 
 echo -e "${color} Copy Expense Config file \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>"$log_file"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 status_check
 
 echo -e "${color} Download Frontend Application code \e[0m"
-rm -rf /usr/share/nginx/html/* &>>"$log_file"
+rm -rf /usr/share/nginx/html/* &>>$log_file
 status_check
 
 echo -e "${color} Download Frontend Application code \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>"$log_file"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 status_check
 
 echo -e "${color} Extract downloaded Application content \e[0m"
-cd /usr/share/nginx/html &>>"$log_file"
-unzip /tmp/frontend.zip &>>"$log_file"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
 status_check
 
 echo -e "${color} Starting Nginx Service \e[0m"
-systemctl enable nginx &>>"$log_file"
-systemctl start nginx &>>"$log_file"
+systemctl enable nginx &>>$log_file
+systemctl start nginx &>>$log_file
 status_check
